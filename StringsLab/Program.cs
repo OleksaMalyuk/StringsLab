ba80fdef7c1b15e3570587779d373b2fa6554ef5
Sasha Malyuk <malyuk.sandra@gmail.com>
2016-05-24 00:54:36 +0300

    Добавлен производный класс и реализовано вычитание строк

--- StringsLab/Program.cs
+++ StringsLab/Program.cs
@@ -49,14 +49,71 @@ namespace StringsLab
         }
      }
 
+    class StringDecimal : StringBase                        // производный класс "Десятичная строка" от класса "Строка"
+    {
+        protected int number_value;                         // содержит числовое выражение строки (для совершения арифм. операций)
+        public StringDecimal()                              // конструктор по умолчанию
+        {
+            this.number_value = 0;
+            this.value = "0";
+        }
+        public StringDecimal(char ch)                       // конструктор из символа                       
+        {
+            this.number_value = (int)Char.GetNumericValue(ch);
+            if (this.number_value == -1)
+                this.number_value = 0;
+            this.value = Convert.ToString(this.number_value);
+        }
+        public StringDecimal(string s)                      // конструктор из строки
+        {
+            try
+            {
+                this.number_value = Convert.ToInt32(s);
+            }
+            catch
+            {
+                this.number_value = 0;
+            }
+            this.value = Convert.ToString(number_value);
+        }
+        public StringDecimal(int number)                    // конструктор из числа
+        {
+            this.number_value = number;
+            this.value = Convert.ToString(number);
+        }
+
+        override public bool Equals(object obj)             // проверяет арифметическое равенство строк (как два числа)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+            StringDecimal temp = (StringDecimal)obj;
+            return number_value == temp.number_value;
+        }
+        override public int GetHashCode()
+        {
+            return number_value.GetHashCode();
+        }
+         public int CompareTo(StringDecimal s2)           // Сравнивает строку по значению с другой как два числа
+        {
+            if (this.number_value > s2.number_value) return 1;
+            if (this.number_value < s2.number_value) return -1;
+            return 0;
+        }
+        public StringDecimal Subtraction(StringDecimal s2)      // Арифметическое вычитание
+        {
+            return new StringDecimal(this.number_value - s2.number_value);
+        }
+    }
 
 
     class Program
     {
         static void Main(string[] args)
         {
-            StringBase s1 = new StringBase("30");
-            StringBase s2 = new StringBase("20");
+            StringDecimal s1 = new StringDecimal(30);
+            StringDecimal s2 = new StringDecimal("-20");
+            StringDecimal s3 = s1.Subtraction(s2);
+            Console.WriteLine("\"{0}\" - \"{1}\" = \"{2}\"", s1, s2, s3);
 
         }
     }
